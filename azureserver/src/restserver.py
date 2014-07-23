f0a6cf51040ff5cd3fb50c7f479ec26ada8e7aef
kehl <kangjihua_home@126.com>
2014-07-23 16:47:17 +0800

    three step connection: 1.Clients ask for a server 2.Process initiat 3.Shutdown the Process

--- azureserver/src/restserver.py
+++ azureserver/src/restserver.py
@@ -1,21 +1,43 @@
 #coding=utf-8
-import paramiko
+import paramiko,datetime
 from xml.etree.ElementTree import ElementTree
 class Usage:
     def __init__(self):
         pass
     
 class RestServer():
-    def __init__(self,path):
+    def __init__(self,lab_vm_path,host_authentication_path):
         self.__lab_set=set()
         self.__lab_virtual_machine_dict = dict()
+        self.__host_authentication_dict = dict()
         self.__user_virtual_machine_dict = dict()
-        self.__read_config(path)
+        self.__read_lab_vm_config(lab_vm_path)
+        self.__read_host_authentication_config(host_authentication_path)
         self.__max_iter=100
+        self.created_time = datetime.datetime.now() 
     
-    def __read_config(self,path):
+    def __read_host_authentication_config(self,path):
         '''
-        initial the lab and virtual machine map
+        '''
+        tree = ElementTree()
+        tree.parse(path)
+        root = tree.getroot()
+        hosts = root.getchildren()
+        for host in hosts:
+            host_name = host.attrib['name']
+            for role in host:
+                vm = host_name
+                auth=dict()
+                for item in role:
+                    if item.tag=='port':
+                        vm = vm + ':' + item.text
+                    else:
+                        auth[item.tag] = item.text
+                self.__host_authentication_dict[vm] = auth
+        
+    def __read_lab_vm_config(self,path):
+        '''
+        Initial the lab and virtual machine map
         '''
         tree = ElementTree()
         tree.parse(path)
@@ -26,21 +48,21 @@ class RestServer():
             if (lab_name in self.__lab_set) ==False:
                 self.__lab_set.add(lab_name)
                 self.__lab_virtual_machine_dict[lab_name]=[]
-            vms = lab.findall('host')
-            for vm in vms:
-                tmp={}
-                info = vm.getchildren()
-                for i in range(len(info)):
-                    tmp[info[i].tag] = info[i].text
-                self.__lab_virtual_machine_dict[lab_name].append(tmp)
+            for host in lab:
+                host_name = host.attrib['name']
+                for port in host:
+                    virtual_machine = dict()
+                    virtual_machine['hostname'] = host_name
+                    virtual_machine['port'] = port.text
+                    self.__lab_virtual_machine_dict[lab_name].append(virtual_machine)
+    
+    def __check_process_existence(self,client,pid):
+        stdin,stdout,stderr = client.exec_command('ps -p '+str(pid)+' -f | grep '+str(pid))
+        res = stdout.read()
+        if len(res)>0:
+            return True
+        return False
         
-    def get_virtual_machine(self,lab):
-        if (lab in self.__lab_set)==True:
-            vms = self.__lab_virtual_machine_dict[lab]
-            vms[0]['lab']=lab
-            return vms[0]
-        else:
-            return None
     def __create_ssh_client(self,host,username,password,port):
         try:
             client = paramiko.SSHClient()
@@ -52,37 +74,55 @@ class RestServer():
             client=None
         finally:
             return client
-    #Later TO DO
-    def resource_monitor(self,host,username,password,port):
-        try:
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(host, port, username, password);
-            return client
-        except Exception, e:
-            print 'Error Connecting : '+str(e)
+         
+    def __get_virtual_machine_authentication(self,hostname,port):
+        '''
+        Return the username and password according to the hostname and port 
+        '''
+        return 'opentech','@dministrat0r'
     
-    def record_user_virtual_machine_info(self,info):
-        #client_id,hostname,pid,lab_name
-        self.__user_virtual_machine_dict[info[0]] = info[1:]
+    def get_virtual_machine_by_lab(self,lab):
+        if (lab in self.__lab_set)==True:
+            vms = self.__lab_virtual_machine_dict[lab]
+            whole_host_name = vms[0]['hostname']+':'+vms[0]['port']
+            #hard code
+            #return the first virtual machine
+            #actually I need to check the status and usage for all the candidate virtual machines
+            # and then pick one of them 
+            temp = self.__host_authentication_dict[whole_host_name] 
+            vms[0]['lab']=lab
+            vms[0].update(temp)
+            return vms[0]
+        else:
+            return None
+    
+    def record_user_virtual_machine_info(self,client_id,hostname,port,pid,lab_name):
+        #client_id,hostname,port,pid,lab_name
+        if client_id in self.__user_virtual_machine_dict:
+            return False
+        info = dict()
+        info['hostname'] = hostname
+        info['port'] = port
+        info['pid'] = pid
+        info['lab_name'] = lab_name 
+        self.__user_virtual_machine_dict[client_id] = info
         return True
-    def __check_process_existence(self,client,pid):
-        stdin,stdout,stderr = client.exec_command('ps -p '+str(pid)+' -f | grep '+str(pid))
-        res = stdout.read()
-        if len(res)>0:
-            return True
-        return False
-        
+    
+    def get_virtual_machine_info(self,client_id):
+        tmp = self.__user_virtual_machine_dict[client_id]
+        return tmp['hostname'],tmp['port'],tmp['pid'],tmp['lab_name']
+    
     def kill_process(self,client_id):
-        #get the host name, username, password and port
-        #kill the pid with paramiko
-        #if any exception, iter this flow for no more than self.__max_iter times
-        hostname=None
-        username=None
-        password=None
-        port = None
-        pid=None
-        client = self.__create_ssh_client(hostname, username, password, port)
+        '''
+        Get the host name, username, password and port by client_id
+        Kill the pid with paramiko
+        If any exception, iter this flow for no more than self.__max_iter times
+        '''
+
+        hostname,port,pid,lab_name = self.get_virtual_machine_info(client_id)
+        username,password = self.__get_virtual_machine_authentication(hostname,port)
+        
+        client = self.__create_ssh_client(hostname, username, password, int(port))
         cur_iter=0
         while client==None and cur_iter<self.__max_iter:
             client = self.__create_ssh_client(hostname, username, password, port)
@@ -91,20 +131,24 @@ class RestServer():
         while True and cur_iter<self.__max_iter:
             client.exec_command('kill '+pid)
             pid_existence = self.__check_process_existence(client, pid)
-            cur_iter = iter+1
+            cur_iter = cur_iter+1
             if pid_existence==False:
                 break
         client.close()
-    
+
+    #Later TO DO
+    def resource_monitor(self,host,username,password,port):
+        pass
+
 if __name__=='__main__':
-    rs = RestServer('/home/kehl/workspace/OSSLab/conf/lab_vm.xml')
-    
-    host = rs.get_virtual_machine('python')
+    rs = RestServer('/home/kehl/workspace/OSSLab/conf/lab_vm.xml','/home/kehl/authentication.xml')
+    host = rs.get_virtual_machine_by_lab('python')
     print host['lab']
     print host['hostname']
     print host['username']
+    print host['password']
     print host['port']
-    rs.kill_process(16666)
+    #rs.kill_process(16666)
     
     
     
--- azureserver/src/webpy.py
+++ azureserver/src/webpy.py
@@ -1,54 +1,64 @@
 #coding=utf-8
 import web,json,restserver
 urls = ('/(.*)', 'index')
-
+rs = restserver.RestServer('/home/kehl/workspace/OSSLab/conf/lab_vm.xml','/home/kehl/workspace/OSSLab/conf/authentication.xml')
 class index:
     def __init__(self):
-        self.restserver = restserver.RestServer('/home/kehl/workspace/OSSLab/conf/lab_vm.xml')
+        pass
     def parse(self,uri):
         '''
         uri[0]:tag
         uri[1]:lab
         uri[2]:connection type
         '''
+        global rs
         if uri[0]=='1':#ask for a server
             if(len(uri)!=3):
                 return -1;
-            
+            vm = rs.get_virtual_machine_by_lab(uri[1])
             return 1
+            if vm==None:
+                return -1
         elif uri[0]=='2':#record the PID, the session and the host name
-            if(len(uri)!=5):
+            if(len(uri)!=6):
                 return -1;
-            result = self.restserver.record_user_virtual_machine_info(uri[1:5])
+            #client_id, host name, port, pid, lab name
+            result = rs.record_user_virtual_machine_info(uri[1],uri[2],uri[3],uri[4],uri[5])
             if result==True:
                 return 2
             return -1
         elif uri[0]=='3':#receive a shutdown signal and kill the PID correspond to the session
             if(len(uri)!=2):
                 return -1
-            
+            rs.kill_process(uri[1])
             return 3
+        elif uri[0]=='4':
+            #re-load the rest server
+            rs = restserver.RestServer('/home/kehl/workspace/OSSLab/conf/lab_vm.xml')
+            return 4
         else:
             return -1
         
     def GET(self,info):
-        #do some thingi
         web.input(info=None)
+        #shit=web.ctx.items()
+        #print shit
         info = str(info)
-        print info
         info = info.split('_')
-        print info
         result = self.parse(info)
-        if result==True:
-            pyDict = self.restserver.get_virtual_machine(info[0])
+        if result==1:
+            pyDict = rs.get_virtual_machine_by_lab(info[1])
             web.header('Content-Type', 'application/json')
             return json.dumps(pyDict)
+        elif result==2:
+            pass
+        elif result==3:
+            pass
+        elif result==4:
+            return rs.fuck
         else:
             return 'Illegal Request'
-        #if pyDict==None:
-            #create a virtual machine and record it into the lab_vm.xml 
-         #   pass
-            
+        
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
